06e68e6a453ad6234fe19a07f07074e034119887
zhangao0605 <zhangao@digvita.com>
2019-11-13 19:03:27 +0800

    11-13

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Button, Table} from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+            dataSource: [
+                {
+                    key: '1',
+                    name: '胡彦斌',
+                    age: 32,
+                    address: '西湖区湖底公园1号',
+                },
+                {
+                    key: '2',
+                    name: '胡彦祖',
+                    age: 42,
+                    address: '西湖区湖底公园1号',
+                },
+            ],
+            columns: [
+                {
+                    title: '姓名',
+                    dataIndex: 'name',
+                    key: 'name',
+                },
+                {
+                    title: '年龄',
+                    dataIndex: 'age',
+                    key: 'age',
+                },
+                {
+                    title: '住址',
+                    dataIndex: 'address',
+                    key: 'address',
+                },
+            ],
+        }
+
+    }
+
+    clickme = () => {
+        console.log(this)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Table dataSource={this.state.dataSource} columns={this.state.columns}/>;
+                    <Button onClick={this.clickme} type="primary">Button</Button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
